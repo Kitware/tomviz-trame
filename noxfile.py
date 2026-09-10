9867dd1410b66e8c8938f99eb9126c8cc773c05d
Alessandro Genova <alessandro.genova@kitware.com>
2026-09-10 09:55:31 -0400

    chore(nox): fix the tests extra, drop parsli leftovers

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import argparse
 import shutil
 from pathlib import Path
 
@@ -22,17 +21,6 @@ def lint(session: nox.Session) -> None:
     session.run(
         "pre-commit", "run", "--all-files", "--show-diff-on-failure", *session.posargs
     )
-
-
-# @nox.session
-# def pylint(session: nox.Session) -> None:
-#     """
-#     Run PyLint.
-#     """
-#     # This needs to be installed into the package environment, and is slower
-#     # than a pre-commit check
-#     session.install(".", "pylint>=3.2")
-#     session.run("pylint", "parsli", *session.posargs)
 
 
 @nox.session(venv_backend="none")
@@ -63,57 +51,8 @@ def tests(session: nox.Session) -> None:
     """
     Run the unit and regular tests.
     """
-    session.install(".[test]")
+    session.install(".[dev]")
     session.run("pytest", *session.posargs)
-
-
-@nox.session(reuse_venv=True)
-def docs(session: nox.Session) -> None:
-    """
-    Build the docs. Pass --non-interactive to avoid serving. First positional argument is the target directory.
-    """
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-b", dest="builder", default="html", help="Build target (default: html)"
-    )
-    parser.add_argument("output", nargs="?", help="Output directory")
-    args, posargs = parser.parse_known_args(session.posargs)
-    serve = args.builder == "html" and session.interactive
-
-    session.install("-e.[docs]", "sphinx-autobuild")
-
-    shared_args = (
-        "-n",  # nitpicky mode
-        "-T",  # full tracebacks
-        f"-b={args.builder}",
-        "docs",
-        args.output or f"docs/_build/{args.builder}",
-        *posargs,
-    )
-
-    if serve:
-        session.run("sphinx-autobuild", "--open-browser", *shared_args)
-    else:
-        session.run("sphinx-build", "--keep-going", *shared_args)
-
-
-@nox.session
-def build_api_docs(session: nox.Session) -> None:
-    """
-    Build (regenerate) API docs.
-    """
-
-    session.install("sphinx")
-    session.run(
-        "sphinx-apidoc",
-        "-o",
-        "docs/api/",
-        "--module-first",
-        "--no-toc",
-        "--force",
-        "src/parsli",
-    )
 
 
 @nox.session
